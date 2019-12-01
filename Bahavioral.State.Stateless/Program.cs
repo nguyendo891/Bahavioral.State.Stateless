d9e78fa024e07859a33404ecd9d27b907b568243
dodinhnguyen <nguyendo891@yahoo.com>
2019-12-01 15:53:22 +0100

    Add project files.

---
+++ Bahavioral.State.Stateless/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Stateless;
+
+
+namespace Bahavioral.State.Stateless
+{
+    public enum Health
+    {
+        NonReproductive,
+        Pregnant,
+        Reproductive
+    }
+
+    public enum Activity
+    {
+        GiveBirth,
+        ReachPuberty,
+        HaveAbortion,
+        HaveUnprotectedSex,
+        Historectomy
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stateMachine = new StateMachine<Health, Activity>(Health.NonReproductive);
+            stateMachine.Configure(Health.NonReproductive)
+              .Permit(Activity.ReachPuberty, Health.Reproductive);
+            stateMachine.Configure(Health.Reproductive)
+              .Permit(Activity.Historectomy, Health.NonReproductive)
+              .PermitIf(Activity.HaveUnprotectedSex, Health.Pregnant,
+                () => ParentsNotWatching);
+            stateMachine.Configure(Health.Pregnant)
+              .Permit(Activity.GiveBirth, Health.Reproductive)
+              .Permit(Activity.HaveAbortion, Health.Reproductive);
+        }
+        public static bool ParentsNotWatching
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
